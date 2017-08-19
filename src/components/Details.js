0a90a10b16f481ccade1c803bb3d530960a03314
Barakmichaeli <barak6546@gmail.com>
2017-08-19 14:56:28 +0300

    bugs fixed

--- src/components/Details.js
+++ src/components/Details.js
@@ -5,16 +5,16 @@ class Details extends Component {
     render() {
         return (
             <div className="details-box">
-                <p><u>ID:</u> {this.props.currentPerson.id}</p>
-                <p><u>Name:</u> {this.props.currentPerson.name}</p>
-                <p><u>Grade:</u>{this.props.currentPerson.grade}</p>
-                <p><u>Email:</u> {this.props.currentPerson.email}</p>
-                <p><u>Date Joined:</u> {this.props.currentPerson.date}</p>
-                <p><u>Address:</u> {this.props.currentPerson.address}</p>
-                <p><u>City:</u> {this.props.currentPerson.city}</p>
-                <p><u>Country:</u> {this.props.currentPerson.country}</p>
-                <p><u>ZIP:</u> {this.props.currentPerson.zip}</p>
-                <p><u>Subject</u> {this.props.currentPerson.subject}</p>
+                <p><u>ID: </u> {this.props.currentPerson.id}</p>
+                <p><u>Name: </u> {this.props.currentPerson.name}</p>
+                <p><u>Grade: </u>{this.props.currentPerson.grade}</p>
+                <p><u>Email: </u> {this.props.currentPerson.email}</p>
+                <p><u> </u> {this.props.currentPerson.date}</p>
+                <p><u> </u> {this.props.currentPerson.address}</p>
+                <p><u>City: </u> {this.props.currentPerson.city}</p>
+                <p><u>Country: </u> {this.props.currentPerson.country}</p>
+                <p><u>ZIP: </u> {this.props.currentPerson.zip}</p>
+                <p><u>Subject: </u> {this.props.currentPerson.subject}</p>
 
                 {(this.props.currentPerson) ?
                     <div className="details-btn">
